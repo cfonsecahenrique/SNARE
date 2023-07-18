ab9294ec2c7c2c948d6f83b01b71765af3aef996
Henrique <henrique.c.fonseca@tecnico.ulisboa.pt>
2023-07-18 11:05:59 +0100

    results of heatmaps

--- jobmanager.sh
+++ jobmanager.sh
@@ -7,11 +7,13 @@ while read -r line
 do
 	# Check the current number of running jobs
 	num_jobs=$(squeue -h | wc -l)
+	echo "Jobs already running: $num_jobs"
 	# Wait until the number of running jobs is below the threshold
 	while [[ $num_jobs -ge $threshold ]]
 	do
 		sleep 60
 		num_jobs=$(squeue | wc -l)
+		echo "waiting, $num_jobs currently running"
 	done
 	echo "Line: $line"
 	# Generate temp script
@@ -21,8 +23,14 @@ do
 	# Add executable permission for the script
 	chmod +x $script_name
 	echo "Added permission for $script_name"
-	# Submit the job using the sbatch command
-	sbatch $script_name
+	run=1
+	while [[ $run -le 50 ]]
+	do
+		echo "Run number $run"
+		# Submit the job using the sbatch command
+		sbatch -J "job_$run" $script_name
+		((run++))
+	done
 	# Remove temp script
 	rm $script_name
 done < jobs.txt
